0732487fc8a5b6d6ab3f8525a8452c0166be8d43
flyingfishzxf <flyingfish@gmail.com>
2018-07-12 06:46:23 +0000

    触发激活邮件

---
+++ app/Notifications/EmailVerificationNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Cache;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Str;
+
+class EmailVerificationNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * 我们只需要通过邮件通知，因此这里只需要一个 mail 即可
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * 发送邮件时会调用此方法来构建邮件内容，参数就是 App\Models\User 对象
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        // 使用 Laravel 内置的 Str 类生成随机字符串的函数，参数就是要生成的字符串长度
+        $token = Str::random(16);
+        // 往缓存中写入这个随机字符串，有效时间为 30 分钟
+        Cache::set('email_verification_' . $notifiable->email, $token, 30);
+        $url = route('email_verification.verify', ['email' => $notifiable->email, 'token' => $token]);
+        return (new MailMessage)
+            ->greeting($notifiable->name . '您好：')
+            ->subject('注册成功，请验证您的邮箱')
+            ->line('请点击下方链接验证您的邮箱')
+            ->action('验证', url('/'));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
     // 开始
     Route::group(['middleware' => 'email_verified'], function () {
-        Route::get('/test', function () {
-            return 'Your email is verified';
-        });
+        
     });
 });

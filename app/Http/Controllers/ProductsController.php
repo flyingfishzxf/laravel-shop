d0f241dfdc9c077560afab19a91cdc4fcf048d19
flyingfishzxf <flyingfish@gmail.com>
2018-07-13 06:54:45 +0000

    用户界面 - 商品列表

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    /**
+     * 商品列表
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index(Request $request)
+    {
+        $products = Product::query()->where('on_sale', true)->paginate(16);
+
+        return view('products.index', ['products' => $products]);
+    }
+}

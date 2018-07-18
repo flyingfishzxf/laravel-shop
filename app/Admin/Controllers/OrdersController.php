b63a433d3dea747db95f4a0d726f89708c6bbf6f
flyingfishzxf <flyingfish@gmail.com>
2018-07-18 03:09:24 +0000

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -16,8 +16,7 @@ class OrdersController extends Controller
     use ModelForm;
 
     /**
-     * Index interface.
-     *
+     * 订单列表
      * @return Content
      */
     public function index()
@@ -26,6 +25,20 @@ class OrdersController extends Controller
 
             $content->header('订单列表');
             $content->body($this->grid());
+        });
+    }
+
+    /**
+     * 订单详情
+     * @param Order $order
+     * @return Content
+     */
+    public function show(Order $order)
+    {
+        return Admin::content(function (Content $content) use ($order) {
+            $content->header('查看订单');
+            // body 方法可以接受 Laravel 的视图作为参数
+            $content->body(view('admin.orders.show', ['order' => $order]));
         });
     }
 
@@ -90,6 +103,7 @@ class OrdersController extends Controller
                 // 禁用删除和编辑按钮
                 $actions->disableDelete();
                 $actions->disableEdit();
+                $actions->append('<a class="btn btn-xs btn-primary" href="' . route('admin.orders.show', [$actions->getKey()]) .'">查看</a>');
             });
             $grid->tools(function ($tools) {
                 // 禁用批量删除按钮
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -20,5 +20,6 @@ Route::group([
     $router->put('products/{id}', 'ProductsController@update');
 
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
 
 });

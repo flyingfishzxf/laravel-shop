325f52c3f2bb135f1ed5aee00b0e04997e24ad8e
flyingfishzxf <flyingfish@gmail.com>
2018-07-21 03:54:58 +0000

    优化优惠券模块

--- app/Http/Controllers/CouponCodesController.php
+++ app/Http/Controllers/CouponCodesController.php
@@ -4,23 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Exceptions\CouponCodeUnavailableException;
 use App\Models\CouponCode;
+use Illuminate\Http\Request;
 
 class CouponCodesController extends Controller
 {
     /**
      * 查询优惠券
      * @param $code
+     * @param Request $request
      * @return mixed
      * @throws CouponCodeUnavailableException
      */
-    public function show($code)
+    public function show($code, Request $request)
     {
         // 判断优惠券是否存在
         if (!$record = CouponCode::where('code', $code)->first()) {
             throw new CouponCodeUnavailableException('优惠券不存在');
         }
 
-        $record->checkAvailable();
+        $record->checkAvailable($request->user());
 
         return $record;
     }
--- app/Models/CouponCode.php
+++ app/Models/CouponCode.php
@@ -72,10 +72,11 @@ class CouponCode extends Model
 
     /**
      * 检查优惠券可用性
+     * @param User $user
      * @param null $orderAmount
      * @throws CouponCodeUnavailableException
      */
-    public function checkAvailable($orderAmount = null)
+    public function checkAvailable(User $user,$orderAmount = null)
     {
         if (!$this->enabled) {
             throw new CouponCodeUnavailableException('优惠券不存在');
@@ -95,6 +96,20 @@ class CouponCode extends Model
 
         if (!is_null($orderAmount) && $orderAmount < $this->min_amount) {
             throw new CouponCodeUnavailableException('订单金额不满足该优惠券最低金额');
+        }
+
+        $used = Order::where('user_id', $user->id)
+            ->where('coupon_code_id', $this->id)
+            ->where(function ($query) {
+                $query->where(function ($query) {
+                    $query->whereNull('paid_at')->where('closed', false);
+                })->orWhere(function ($query) {
+                    $query->whereNotNull('paid_at')
+                        ->where('refund_status', Order::REFUND_STATUS_PENDING);
+                });
+            })->exists();
+        if ($used) {
+            throw new CouponCodeUnavailableException('你已经使用过这张优惠券了');
         }
     }
 
